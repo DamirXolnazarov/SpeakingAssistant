5704d555e6bf280bc95de9c528220b2752ab4445
unknown <damir777xolnazarov@gmail.com>
2023-10-28 21:59:58 +0500

    28.10.2023

--- src/views/Part1.vue
+++ src/views/Part1.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
-        
+    <div class="part1">
+        <div class="topBar flex flex-col bg-[greenyellow] text-[25px] text-black justify-center items-center w-[100%] h-[100px]">
+           <span>PART 1</span>
+           <span>30 QUESTIONS</span>
+        </div>
+        <div class="questions w-[100%] px-[30px] pt-[30px] flex flex-col justify-center items-start">
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+        </div>
     </div>
 </template>
 
@@ -25,5 +48,7 @@ export default {
 </script>
 
 <style scoped>
-
+.part1{
+    overflow: hidden;
+}
 </style>
--- src/views/Part2.vue
+++ src/views/Part2.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
-        
+        <div class="part2">
+        <div class="topBar flex flex-col bg-[aqua] text-[25px] text-black justify-center items-center w-[100%] h-[100px]">
+           <span>PART 2</span>
+           <span>30 QUESTIONS</span>
+        </div>
+        <div class="questions w-[100%] px-[30px] pt-[30px] flex flex-col justify-center items-start">
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+            <div class="que w-[100%] h-[100px] bg-[red] mb-[30px] rounded-[15px]"></div>
+        </div>
     </div>
 </template>
 
